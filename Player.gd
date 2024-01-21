3b1a3f946eb38a9b707caa1b9e11eaad3d2b9026
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2024-01-21 09:41:32 -0800

    Add project files

---
+++ Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+#onready var player := $Player
+onready var move_timer := $MoveTimer
+
+# "8x8 cell
+var cell_size := 48
+# "16x16" unit
+var unit_size := 96
+var spawn_point := Vector2(4.5, 4.5)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Note: You cannot set this value with time_left(). To change the timer's remaining time, use start().
+	# Set move timer to 0.0
+	move_timer.start(0.0)
+	# Set player starting position
+	position = spawn_point * unit_size
+
+func _process(_delta: float) -> void:
+	print(move_timer.time_left)
+	if move_timer.time_left == 0.0:
+		move_player()
+	
+	
+func move_player():
+	var direction := Vector2.ZERO
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction != Vector2(0.0, 0.0):
+		position += direction * unit_size
+		move_timer.start()
+	
